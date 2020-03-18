ca6555d2e2f80354de841aaa29b62422699b660b
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-03-18 07:52:38 -0700

    Initial setup of Alice

--- mix.exs
+++ mix.exs
@@ -14,15 +14,24 @@ defmodule BebopAlice.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      applications: [:alice],
+      mod: {
+        Alice, %{
+          handlers: [
+            Alice.Handlers.Roller,
+            Alice.Handlers.GoodMorningLanguages
+          ]
+        }
+      }
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:alice,              "~> 0.3.7"},
+      {:alice_roller,       "~> 0.2"},
+      {:alice_good_morning, "~> 1.0"}
     ]
   end
 end
