08591aa33bdf0158d0b71277cac1454f51358f3f
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-06-26 16:09:44 -0700

    Added Jedi Alice

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule BebopAlice.MixProject do
           handlers: [
             Alice.Handlers.Roller,
             Alice.Handlers.GoodMorningLanguages,
-            Alice.Handlers.Karma
+            Alice.Handlers.Karma,
+            Alice.Handlers.Jedi
           ]
         }
       }
@@ -30,10 +31,11 @@ defmodule BebopAlice.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:alice,              "~> 0.4.2", override: true},
+      {:alice,              "~> 0.4.3", override: true},
       {:alice_roller,       "~> 0.3.0"},
       {:alice_good_morning, "~> 1.0"},
-      {:alice_karma,        "~> 0.3.5"}
+      {:alice_karma,        "~> 0.3.5"},
+      {:jedi_alice,         "~> 0.1.0"}
     ]
   end
 end
